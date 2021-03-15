3f21af8a8594e96f09acc8680234605bd56a75de
Jim Madden <jmadden@twilio.com>
2021-03-15 15:10:00 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import TaskQueueAgentAvailabilityPlugin from './TaskQueueAgentAvailabilityPlugin';
+
+FlexPlugin.loadPlugin(TaskQueueAgentAvailabilityPlugin);
